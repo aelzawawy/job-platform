275466ba6138faa043cc99a92066c870f2db6865
Ahmed Ali <aelzawawy45@gmail.com>
2023-04-22 12:25:36 +0200

    newURL

--- src/app/services/jobs.service.ts
+++ src/app/services/jobs.service.ts
@@ -1,91 +1,92 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from '../interfaces/user';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 import { JobPost } from 'src/app/interfaces/job-post';
+import { User } from '../interfaces/user';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JobsService {
-  constructor(private http:HttpClient) { }
-  url:string= 'http://localhost:3000/';
-  socket = io('http://localhost:3000/');
+  constructor(private http: HttpClient) {}
+  url: string = 'https://inreach-api.onrender.com/';
+  socket = io('https://inreach-api.onrender.com/');
   public saveRm$: BehaviorSubject<string> = new BehaviorSubject('');
   public job$: BehaviorSubject<JobPost> = new BehaviorSubject({});
 
-  getJobs(body:any){
-    return this.http.post(this.url + 'jobs-feed', body)
-  };
-
-  public passJob = (res:any) => {
-    this.job$.next(res);
+  getJobs(body: any) {
+    return this.http.post(this.url + 'jobs-feed', body);
   }
+
+  public passJob = (res: any) => {
+    this.job$.next(res);
+  };
   getPassedJob = () => {
     return this.job$.asObservable();
-  }
-  jobById(id:any){
-    return this.http.get(this.url + 'jobs-feed/' + id)
-  }
-
-  getPosts(){
-    return this.http.get(this.url + 'posts/')
   };
-
-  postJob(body:any){
-    return this.http.post(this.url + 'posts/', body)
-  }
-  updateJOb(id:any, body:any){
-    return this.http.patch(this.url + 'posts/' + id, body)
-  }
-  delJOb(id:any){
-    return this.http.delete(this.url + 'posts/' + id)
-  }
-  applyJob(id:any){
-    return this.http.get(this.url + 'apply/' + id)
+  jobById(id: any) {
+    return this.http.get(this.url + 'jobs-feed/' + id);
   }
 
-  checkSaved(id:any){
-    return this.http.get(this.url + 'check_saved/' + id)
-  }
-  saveJob(id:any){
-    return this.http.get(this.url + 'save/' + id)
-  }
-  unSaveJob(id:any){
-    return this.http.get(this.url + 'unSave/' + id)
+  getPosts() {
+    return this.http.get(this.url + 'posts/');
   }
 
+  postJob(body: any) {
+    return this.http.post(this.url + 'posts/', body);
+  }
+  updateJOb(id: any, body: any) {
+    return this.http.patch(this.url + 'posts/' + id, body);
+  }
+  delJOb(id: any) {
+    return this.http.delete(this.url + 'posts/' + id);
+  }
+  applyJob(id: any) {
+    return this.http.get(this.url + 'apply/' + id);
+  }
 
-  users = []
-  currUser:User = {}
+  checkSaved(id: any) {
+    return this.http.get(this.url + 'check_saved/' + id);
+  }
+  saveJob(id: any) {
+    return this.http.get(this.url + 'save/' + id);
+  }
+  unSaveJob(id: any) {
+    return this.http.get(this.url + 'unSave/' + id);
+  }
+
+  users = [];
+  currUser: User = {};
   jobId = '';
-  applicationId = []
-  toUpdate:JobPost = {}
-  getJobInfo(id:any, applicationId:any){
+  applicationId = [];
+  toUpdate: JobPost = {};
+  getJobInfo(id: any, applicationId: any) {
     this.jobId = id;
-    this.applicationId = applicationId.map((el:any) => el._id);
+    this.applicationId = applicationId.map((el: any) => el._id);
   }
 
-  getApplications(users:any){
+  getApplications(users: any) {
     this.users = users;
   }
-  
-  getUser(user:any){
+
+  getUser(user: any) {
     this.currUser = user;
   }
 
-  acceptOffer(id:any, appId:any){
-    return this.http.get(this.url + 'accept/' + id + '/' + appId)
+  acceptOffer(id: any, appId: any) {
+    return this.http.get(this.url + 'accept/' + id + '/' + appId);
   }
 
-  declineOffer(id:any, appId:any){
-    return this.http.get(this.url + 'decline/' + id + '/' + appId)
+  declineOffer(id: any, appId: any) {
+    return this.http.get(this.url + 'decline/' + id + '/' + appId);
   }
 
-  searchApi(body:any){
-    return this.http.post(`${this.url}api-search`, body)
+  searchApi(body: any) {
+    return this.http.post(`${this.url}api-search`, body);
   }
-  geoSearch(body:any){
-    return this.http.get(`${this.url}api-search/search_terms/${body.search_terms}/location/${body.location}/radius/${body.radius}/unit/${body.unit}/sort/${body.sort}`)
+  geoSearch(body: any) {
+    return this.http.get(
+      `${this.url}api-search/search_terms/${body.search_terms}/location/${body.location}/radius/${body.radius}/unit/${body.unit}/sort/${body.sort}`
+    );
   }
 }
