d482708fbd45dcb24986445596e0f8300926b15f
Ahmed Ali <aelzawawy45@gmail.com>
2023-04-22 10:01:03 +0200

    hosting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'reachJobs-app'`, () => {
+  it(`should have as title 'in_reach'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('reachJobs-app');
+    expect(app.title).toEqual('in_reach');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('reachJobs-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'in_reach app is running!'
+    );
   });
 });
